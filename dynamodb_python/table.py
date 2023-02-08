49f638df1476c2c000188a5518f14503ef2724a8
domas-v <domas.vaitmonas93@gmail.com>
2023-02-08 14:40:46 +0200

    Renamed dir, split logic, added dynamic attributes

--- dynamodb_python/table.py
+++ dynamodb_python/table.py
@@ -1,101 +1,31 @@
+import datetime
 import time
 from dataclasses import dataclass
-from datetime import datetime
-from typing import Any, ClassVar, Dict, Optional, Union
+from typing import Any, Optional, Union
 
 import boto3
 import botocore
-from boto3.dynamodb.conditions import And, Attr, ConditionExpressionBuilder, Key, Not, Or
-from botocore.config import Config
+from boto3.dynamodb.conditions import And, ConditionExpressionBuilder, Key
 from ddbcereal import Deserializer, Serializer
 from ddbcereal.types import PythonNumber
 from requests import RequestException
-from utils import normalize_dynamodb_write, split_list
+from utils import get_logical_operation, make_expression, normalize_dynamodb_write, split_list
 
 
 AUTH_NAME = "SYSTEM"  # TODO: change this
 
 
-def get_logical_operation(op: str):
-    if op == "and":
-        return And
-    elif op == "or":
-        return Or
-    elif op == "not":
-        return Not
-    else:
-        raise RequestException(f"{op} is not a valid logical operator (and, or, not).")
-
-
-def get_operation(op: str):
-    """Operation function"""
-    if op == "eq":
-        return Attr.eq
-    if op == "ne":
-        return Attr.ne
-    elif op == "lt":
-        return Attr.lt
-    elif op == "gt":
-        return Attr.gt
-    elif op == "lte":
-        return Attr.lte
-    elif op == "gte":
-        return Attr.gte
-    elif op == "between":
-        return Attr.between
-    elif op == "begins_with":
-        return Attr.begins_with
-    elif op == "contains":
-        return Attr.contains
-    elif op == "exists":
-        return Attr.exists
-    elif op == "not_exists":
-        return Attr.not_exists
-    elif op == "in":
-        return Attr.is_in
-
-
-def make_expression(filter_expression: dict, is_key: bool = False) -> Attr:
-    """Given {key, op, value} map - make filter expression."""
-    attr = Attr(filter_expression["key"]) if not is_key else Key(filter_expression["key"])
-    op = get_operation(filter_expression["op"])
-    value = filter_expression.get("value", None)
-    if not value:
-        return op(attr)
-    elif isinstance(value, (list, tuple)):
-        return op(attr, *value)
-    else:
-        return op(attr, value)
-
-
-@dataclass
-class DynamoDB:
-    """Base for communications with DynamoDB.
-
-    Raises:
-        KeyError: 'Item' not found in response for _read_item or 'Items' in _read_items.
-    """
-
-    DATE_FORMAT: ClassVar[str] = "%Y%m%d"
-    YEAR_WEEK_FORMAT: ClassVar[str] = "%Y%W"
-    YEAR_MONTH_FORMAT: ClassVar[str] = "%Y%m"
-
-    table_name: str  # TODO: change this a list of tables
-    credentials: Union[Dict, None] = None
-    config: Optional[botocore.client.Config] = None  # TODO: should be able to pass a file
+@dataclass()
+class Table:
+    tablename: str
+    _client: boto3.client
 
     def __post_init__(self):
-        self.__exp_builder = ConditionExpressionBuilder()
-        self.__serializer = Serializer()
-        self.__deserializer = Deserializer(number_type=PythonNumber.INT_OR_DECIMAL)
+        self._exp_builder = ConditionExpressionBuilder()
+        self._serializer = Serializer()
+        self._deserializer = Deserializer(number_type=PythonNumber.INT_OR_DECIMAL)
 
-        self._client = boto3.client(
-            "dynamodb",
-            config=self.config if self.config else None,
-            **self.credentials if self.credentials else {},
-        )
-
-    def __serialize(self, value: Any, explicit_value: bool = False) -> Union[dict, list]:
+    def _serialize(self, value: Any, explicit_value: bool = False) -> Union[dict, list]:
         """Serializes value to DynamoDB structure.
 
         Explicit value indicates that this value (list or dict) is a value.
@@ -103,13 +33,13 @@ class DynamoDB:
         rather than "value".
         """
         if isinstance(value, dict) and not explicit_value:
-            return self.__serializer.serialize_item(value)
+            return self._serializer.serialize_item(value)
         if isinstance(value, list) and not explicit_value:
-            return [self.__serializer.serialize_item(item) for item in value]
+            return [self._serializer.serialize_item(item) for item in value]
         else:
-            return self.__serializer.serialize(value)
+            return self._serializer.serialize(value)
 
-    def __deserialize(self, value: Union[dict, list], explicit_value: bool = False) -> Any:
+    def _deserialize(self, value: Union[dict, list], explicit_value: bool = False) -> Any:
         """Serializes value to DynamoDB structure.
 
         Explicit value indicates that this value (list or dict) is a value.
@@ -117,21 +47,21 @@ class DynamoDB:
         rather than "value".
         """
         if isinstance(value, dict) and not explicit_value:
-            return self.__deserializer.deserialize_item(value)
+            return self._deserializer.deserialize_item(value)
         if isinstance(value, list) and not explicit_value:
-            return [self.__deserializer.deserialize_item(item) for item in value]
+            return [self._deserializer.deserialize_item(item) for item in value]
         else:
-            return self.__deserializer.deserialize(value)
+            return self._deserializer.deserialize(value)
 
     def read_item(self, key: str, time: int) -> dict:
         response = self._client.get_item(
-            TableName=self.table_name, Key=DynamoDB.__serialize({"key": key, "time": time})
+            TableName=self.tablename, Key=self._serialize({"key": key, "time": time})
         )
 
         if "Item" not in response:
             raise KeyError("Key(%s) and time(%s) was not found." % (key, time))
 
-        return self.__deserialize(response["Item"])
+        return self._deserialize(response["Item"])
 
     def read_items(
         self,
@@ -207,14 +137,12 @@ class DynamoDB:
                 filter_exp = conditional_filter_exp
 
             if filter_exp:
-                filter_exp = DynamoDB.__exp_builder.build_expression(
-                    filter_exp, is_key_condition=False
-                )
+                filter_exp = self._exp_builder.build_expression(filter_exp, is_key_condition=False)
                 kwargs["FilterExpression"] = filter_exp.condition_expression
                 kwargs["ExpressionAttributeNames"].update(filter_exp.attribute_name_placeholders)
                 kwargs["ExpressionAttributeValues"].update(filter_exp.attribute_value_placeholders)
 
-        key_exp = self.__exp_builder.build_expression(key_exp, is_key_condition=True)
+        key_exp = self._exp_builder.build_expression(key_exp, is_key_condition=True)
         kwargs.update(
             dict(
                 KeyConditionExpression=key_exp.condition_expression,
@@ -224,7 +152,7 @@ class DynamoDB:
         kwargs["ExpressionAttributeNames"].update(key_exp.attribute_name_placeholders)
         kwargs["ExpressionAttributeValues"].update(key_exp.attribute_value_placeholders)
 
-        kwargs["ExpressionAttributeValues"] = self.__serialize(kwargs["ExpressionAttributeValues"])
+        kwargs["ExpressionAttributeValues"] = self._serialize((kwargs["ExpressionAttributeValues"]))
 
         if limit or limit == 0:
             limit = min(max(0, limit), 200)
@@ -233,11 +161,13 @@ class DynamoDB:
             kwargs["Limit"] = limit * 2
 
         if last:
-            kwargs["ExclusiveStartKey"] = self.__serialize(
-                {
-                    "key": last["key"],
-                    "time": int(last["time"]),
-                }
+            kwargs["ExclusiveStartKey"] = self._serialize(
+                (
+                    {
+                        "key": last["key"],
+                        "time": int(last["time"]),
+                    }
+                )
             )
 
         items = []
@@ -245,8 +175,8 @@ class DynamoDB:
         parse_more = True
 
         while parse_more:
-            response = self._client.query(TableName=self.table_name, **kwargs)
-            items.extend(self.__deserialize(response["Items"]))
+            response = self._client.query(TableName=self.tablename, **kwargs)
+            items.extend(self._deserialize(response["Items"]))
             last_key = response.get("LastEvaluatedKey", {})
             parse_more = ((limit and len(items) < limit) or limit is None) and last_key
             kwargs["ExclusiveStartKey"] = last_key
@@ -255,18 +185,18 @@ class DynamoDB:
             truncated_items = items[:limit]
             last_key = truncated_items[-1]
             return truncated_items, last_key
-        return items, self.__deserialize(last_key)
+        return items, self._deserialize(last_key)
 
     def delete(self, key: str, time: int) -> dict:
         """Delete item and return its values."""
         response = self._client.delete_item(
-            TableName=self.table_name,
-            Key=self.__serialize({"key": key, "time": time}),
+            TableName=self.tablename,
+            Key=self._serialize(({"key": key, "time": time})),
             ReturnValues="ALL_OLD",
         )
         if "Attributes" not in response:
             raise KeyError("Key(%s) and time(%s) was not found." % (key, time))
-        return self.__deserialize(response["Attributes"])
+        return self._deserialize(response["Attributes"])
 
     def delete_list(self, keys: list, time: int) -> dict:
         """Delete same "time" from multiple keys."""
@@ -315,9 +245,9 @@ class DynamoDB:
         if "updated_at" in item:
             data["updated_at"] = item["updated_at"] = int(datetime.utcnow().timestamp())
 
-        normalized_item = self.__serialize(normalize_dynamodb_write(item))
+        normalized_item = self._serialize((normalize_dynamodb_write(item)))
         old_values = self._client.put_item(
-            TableName=self.table_name,
+            TableName=self.tablename,
             Item=normalized_item,
             ReturnValues=return_values,
         )
@@ -325,7 +255,7 @@ class DynamoDB:
         if return_old_values:
             normalized_item = old_values["Attributes"]
 
-        return self.__deserialize(normalized_item)
+        return self._deserialize(normalized_item)
 
     def write_batch(self, items: list, write_op: bool = True) -> None:
         """Write list of items."""
@@ -334,9 +264,9 @@ class DynamoDB:
             return
 
         if write_op:
-            items = [{"PutRequest": {"Item": DynamoDB.__serialize(item)}} for item in items]
+            items = [{"PutRequest": {"Item": self._serialize((item))}} for item in items]
         else:
-            items = [{"DeleteRequest": {"Key": DynamoDB.__serialize(item)}} for item in items]
+            items = [{"DeleteRequest": {"Key": self._serialize((item))}} for item in items]
 
         # If given list exceeds 25 items, need to split it into multiple batch reads,
         # since AWS only supports maximum of 25 items or 16MB batch reads.
@@ -346,12 +276,12 @@ class DynamoDB:
             split_items = [items]
 
         for batch in split_items:
-            response = self._client.batch_write_item(RequestItems={self.table_name: batch})
+            response = self._client.batch_write_item(RequestItems={self.tablename: batch})
 
             # If batch read exceeded limits and returned UnprocessedKeys, add them to read queue.
             unprocessed_keys = response.get("UnprocessedKeys", {})
             if unprocessed_keys:
-                split_items.append(unprocessed_keys[self.table_name]["Keys"])
+                split_items.append(unprocessed_keys[self.tablename]["Keys"])
 
     def write_batch_migrate(self, items: list):
         """Write list of items with write capacity exceptions in mind."""
@@ -373,7 +303,7 @@ class DynamoDB:
         a dict with the key and time of required record in Dynamo."""
         all_responses = []
 
-        items = [self.__serialize(item) for item in items]
+        items = [self._serialize((item)) for item in items]
 
         # If given list exceeds 100 items, need to split it into multiple batch reads,
         # since AWS only supports maximum of 100 items or 16MB batch reads.
@@ -387,16 +317,16 @@ class DynamoDB:
 
         for batch in split_items:
             response = self._client.batch_get_item(
-                RequestItems={self.table_name: {**request_params, "Keys": batch}}
+                RequestItems={self.tablename: {**request_params, "Keys": batch}}
             )
 
-            responses = response["Responses"][self.table_name]
-            all_responses.append(DynamoDB.__deserialize(responses))
+            responses = response["Responses"][self.tablename]
+            all_responses.append(self._deserialize(responses))
 
             # If batch read exceeded limits and returned UnprocessedKeys, add them to read queue.
             unprocessed_keys = response.get("UnprocessedKeys", {})
             if unprocessed_keys:
-                split_items.append(unprocessed_keys[self.table_name]["Keys"])
+                split_items.append(unprocessed_keys[self.tablename]["Keys"])
 
         # Flatten all batch responses into a single list and return.
         return [item for batch in all_responses for item in batch]
@@ -428,13 +358,13 @@ class DynamoDB:
             kwargs["ExpressionAttributeNames"] = attributes
 
         response = self._client.update_item(
-            TableName=self.table_name,
-            Key={"key": self.__serialize(key), "time": self.__serialize(time)},
+            TableName=self.tablename,
+            Key={"key": self._serialize((key)), "time": self._serialize((time))},
             UpdateExpression=f"add {field} :value",
             # TODO This will break if field is reserved keyword.
             #  botocore classes should be used for constructing those time of things.
             ExpressionAttributeValues={
-                ":value": self.__serialize(value, explicit_value=True),
+                ":value": self._serialize(value, explicit_value=True),
             },
             ReturnValues="ALL_NEW",
             **kwargs,
@@ -480,31 +410,19 @@ class DynamoDB:
 
         try:
             response = self._client.update_item(
-                TableName=self.table_name,
-                Key={"key": self.__serialize(key), "time": self.__serialize(time)},
+                TableName=self.tablename,
+                Key={"key": self._serialize((key)), "time": self._serialize((time))},
                 UpdateExpression=f"set {field} = if_not_exists({field}, :value)",
                 # TODO This will break if field is reserved keyword.
                 #  botocore classes should be used for constructing those sort of things.
                 ExpressionAttributeValues={
-                    ":value": self.__serialize({}, explicit_value=True),
+                    ":value": self._serialize({}, explicit_value=True),
                 },
                 ReturnValues="ALL_NEW",
                 **kwargs,
             )
-            return self.__deserialize(response["Attributes"])
+            return self._deserialize(response["Attributes"])
         except botocore.errorfactory.ClientError as e:
             # Failure due to existing attribute ignored on purpose.
             if e.response["Error"]["Code"] != "ConditionalCheckFailedException":
                 raise
-
-
-if __name__ == "__main__":
-    dynamodb = DynamoDB(
-        table_name="test", config=Config(retries={"max_attempts": 5, "mode": "standard"})
-    )
-
-    dynamodb.write(
-        key="test",
-        time=0,
-        attributes={"foo": "bar", "baz": 1, "qux": {"quux": "quuz"}},
-    )
